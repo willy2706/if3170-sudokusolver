137571fdf93ad55ba955b602b5879322c10119c9
arinalistyarini <arinalistyarini@yahoo.com>
2014-10-10 14:08:49 +0700

    tes

--- src/socrates_asjavaapp/Socrates_AsJavaApp.java
+++ src/socrates_asjavaapp/Socrates_AsJavaApp.java
@@ -28,10 +28,10 @@ import jess.Rete;
 public class Socrates_AsJavaApp{
 public static void main(String[] args) {
     try {
-    Rete env = new Rete();
-    env.batch("E:\\PEERSEMESTEREMPAT\\AI\\tubes1\\SocratesKnowledgeBase\\SocratesKnowledgeBase.clp");
-    env.reset();
-    env.run();
+        Rete env = new Rete();
+        env.batch("E:\\PEERSEMESTEREMPAT\\AI\\tubes1\\SocratesKnowledgeBase\\SocratesKnowledgeBase.clp");
+        env.reset();
+        env.run();
     } catch (JessException e) {
     e.printStackTrace();
     }
